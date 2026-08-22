a4a950321862ecab08771929b4f625bd094b4d0f
himalayakathpal <41870226+himalayakathpal@users.noreply.github.com>
2026-08-22 17:12:29 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // while (fast!=null && fast.next!=null) {
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        // }
+        // ListNode secondHalf = slow.next;
+        // slow.next = null;
+
+        // ListNode prev = null;
+
+        // while(secondHalf!=null) {
+        //     ListNode nextNode = secondHalf.next;
+        //     secondHalf.next = prev;
+        //     prev = secondHalf;
+        //     secondHalf = nextNode;
+        // }
+        // secondHalf = prev;
+
+        // ListNode firstHalf = head;
+
+        // while (secondHalf!=null) {  
+        //     ListNode nextNode = secondHalf.next;
+        //     secondHalf.next = firstHalf.next;
+        //     firstHalf.next = secondHalf;
+        //     firstHalf = secondHalf.next;
+        //     secondHalf = nextNode;
+        // }
+
+        reorderListHelper(head, head);
+
+    }
+    public ListNode reorderListHelper (ListNode root, ListNode curr) {
+        if (curr == null) return root;
+
+        root = reorderListHelper(root, curr.next);
+
+        if (root == null) return null;
+
+        ListNode temp = null;
+
+        if (root == curr || root.next == curr) {
+            curr.next = null;
+            return null;
+        } else {
+            temp = root.next;
+            curr.next = temp;
+            root.next = curr;
+        }
+        return temp;
+    }
+}
