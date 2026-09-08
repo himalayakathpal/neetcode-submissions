c3069ad66242f313ee4e41e7a1e273bc1bba5242
himalayakathpal <41870226+himalayakathpal@users.noreply.github.com>
2026-09-08 16:52:54 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,36 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraphHelper(Node node, Map<Node, Node> mp) {
+        if (node == null) return null;
+        if (mp.containsKey(node)) return mp.get(node);
+        Node newNode = new Node(node.val);
+        mp.put(node, newNode);
+        for (Node n: node.neighbors) {
+            newNode.neighbors.add(cloneGraphHelper(n, mp));
+        }
+        return newNode;
+    }    
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> mp = new HashMap<>();
+        return cloneGraphHelper(node, mp);
+    }
+}
