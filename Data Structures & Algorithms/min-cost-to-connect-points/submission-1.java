a22b588757e934e0d755a90100e532daaf8d9b3e
himalayakathpal <41870226+himalayakathpal@users.noreply.github.com>
2026-09-12 01:54:15 +0530

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.java
@@ -0,0 +1,53 @@
+class DisjointSet {
+    int[] rank;
+    int[] parent;
+    int size;
+    DisjointSet(int n) {
+        size = n;
+        parent = new int[size];
+        rank = new int[size];
+        for (int i=0;i<size;i++) {
+            parent[i] = i;
+        }
+    }
+    public int findParent(int n) {
+        if (n == parent[n]) return n;
+        parent[n] = findParent(parent[n]);
+        return parent[n];
+    }
+    public boolean unionByRank(int a, int b) {
+        int parentA = findParent(a);
+        int parentB = findParent(b);
+        if (parentA == parentB) return false;
+        if (rank[parentA] >= rank[parentB]) {
+            rank[parentA]++;
+            parent[parentB] = parentA;
+        } else {
+            rank[parentB]++;
+            parent[parentA] = parentB;
+        }
+        return true;
+    }
+}
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        DisjointSet ds = new DisjointSet(points.length);
+        int count = 0;
+        int result = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2]-b[2]);
+        for (int i = 0;i<points.length;i++) {
+            for (int j = i+1;j<points.length;j++) {
+                int dist = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]); 
+                pq.offer(new int[] {i,j, dist});
+            }
+        }
+        while (count<points.length-1) {
+            int[] edge = pq.poll();
+            if (ds.unionByRank(edge[0],edge[1])) {
+                result+=edge[2];
+                count++;
+            } 
+        }
+        return result;
+    }
+}
